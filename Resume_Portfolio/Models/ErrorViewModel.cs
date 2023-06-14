c8f0e36c6017babfb8389d3b9768ced9f2a83a70
johndward01 <johndward01@gmail.com>
2023-06-14 10:42:27 -0500

    Add project files.

---
+++ Resume_Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Resume_Portfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
